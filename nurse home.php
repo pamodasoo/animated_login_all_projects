ecc7efffd244deaa4233aaf41b7f412967b5bd74
Shehani Ranasinghe <30473140+ShehaniRanasighe@users.noreply.github.com>
2019-01-03 00:09:15 +0530

    commit some changes

---
+++ nurse home.php
@@ -0,0 +1,24 @@
+<?php
+  session_start();
+
+
+include("header.php");
+include("library.php");
+  
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MyTasks</title>
+</head>
+<body>
+	
+<div class="w3-container">
+  <center><h1>My Tasks</h1></center><br><br>
+  <center><div class="w3-card" style="width:50%">
+   <a href="all_appointments"></a><img src="image/appointments.png" alt="Person" style="width: 40%">
+    <div class="w3-container">
+      <h2><b><a href="all_appointments.php">Appointments</a></b></h2>
+      
+    </div></center>
+  </div>
